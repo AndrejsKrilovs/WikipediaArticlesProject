4f4669082ca9ce6921675f66d4a44971baedd92c
Andrejs Krilovs <andrejs.krilovs.0507@gmail.com>
2022-02-24 06:25:16 +0300

    Additional test for company service

--- src/test/java/interview/wikicredit/service/CompanyServiceTest.java
+++ src/test/java/interview/wikicredit/service/CompanyServiceTest.java
@@ -5,8 +5,11 @@ import interview.wikicredit.repository.CompanyRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.dao.DuplicateKeyException;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 public class CompanyServiceTest {
     private final CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
@@ -16,7 +19,7 @@ public class CompanyServiceTest {
     public void getCompanies() {
         Company company1 = new Company(1, "Company 1");
         Company company2 = new Company(2, "Company 2");
-        Mockito.doReturn(List.of(company1, company2)).when(companyRepository.findAll());
+        Mockito.doReturn(List.of(company1, company2)).when(companyRepository).findAll();
 
         List<Company> companies = companyService.getCompanyList();
         Assert.assertFalse("Company list should not be empty!", companies.isEmpty());
@@ -24,8 +27,7 @@ public class CompanyServiceTest {
 
     @Test
     public void getCompanyEmptyList() {
-        Mockito.doReturn(List.of()).when(companyRepository.findAll());
-
+        Mockito.doReturn(List.of()).when(companyRepository).findAll();
         List<Company> companies = companyService.getCompanyList();
         Assert.assertTrue("Company list should be empty!", companies.isEmpty());
     }
@@ -37,12 +39,28 @@ public class CompanyServiceTest {
         Mockito.verify(companyRepository, Mockito.times(1)).save(companyToAdd);
     }
 
-    @Test
+    @Test(expected = DuplicateKeyException.class)
     public void addDuplicateCompany() {
         Company companyToAdd = new Company(1, "CompanyToAdd");
-        Mockito.doReturn(List.of(companyToAdd)).when(companyRepository.findAll());
+        Mockito.doReturn(List.of(companyToAdd)).when(companyRepository).findAll();
         companyService.addCompany(companyToAdd);
-        Mockito.verify(companyRepository, Mockito.times(0)).save(companyToAdd);
     }
 
+    @Test
+    public void findCompanyByName() {
+        Company companyToFind = new Company(1, "CompanyToFind");
+        Mockito.doReturn(Optional.of(companyToFind)).when(companyRepository).findByCompanyName("CompanyToFind");
+
+        Company entity = companyService.findCompanyByName("CompanyToFind");
+        Assert.assertEquals("CompanyToFind", entity.getCompanyName());
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void didNotFindCompanyByName() {
+        Company company1 = new Company(1, "Company 1");
+        Company company2 = new Company(2, "Company 2");
+        Mockito.doReturn(List.of(company1, company2)).when(companyRepository).findAll();
+
+        companyService.findCompanyByName("Company 3");
+    }
 }
